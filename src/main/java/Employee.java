1254416bf380c94de28f945f9c7b188b2c5b53b6
MattiasITHS <mattias.hovde@iths.se>
2021-03-19 09:53:46 +0100

    Final push

---
+++ src/main/java/Employee.java
@@ -0,0 +1,40 @@
+public class Employee {
+    private String firstName;
+    private String lastName;
+    private int age;
+    private double salary;
+    private int companyId;
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+    public String getLastName() {
+        return this.lastName;
+    }
+    public int getAge() {
+        return this.age;
+    }
+    public double getSalary() {
+        return this.salary;
+    }
+    public int getCompanyId() {
+        return this.companyId;
+    }
+    public void setCompanyId(int companyId) {
+        this.companyId = companyId;
+    }
+}
